02e0abda1dd585533805667faa7dbf2a08e7c25b
lucastorterotot <lucas.torterotot@gmail.com>
2020-04-24 10:29:47 +0200

    Delphes output tree post processing step files added

---
+++ Delphes_to_NN/macro.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from optparse import OptionParser
+usage = "usage: %prog [options] <input root file> <output file name>"
+parser = OptionParser(usage=usage)
+parser.add_option("-v", "--verbose", dest = "verbose",
+                                    default=False,
+                                    action='store_true')
+parser.add_option("-N", "--Nmax", dest = "Nmax",
+                                    default=None)
+
+(options,args) = parser.parse_args()
+
+if len(args) == 2:
+    input_file_str = args[0]
+    output_file_str = args[1]
+else:
+    raise RuntimeError("Please give one input file and one output file")
+
+import ROOT
+import libPyROOT as _root
+ROOT.gSystem.Load("libDelphes")
+
+input_file = ROOT.TFile(input_file_str, "READ")
