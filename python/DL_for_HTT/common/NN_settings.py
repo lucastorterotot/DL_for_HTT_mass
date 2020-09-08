de7c6d5d78fbceed423189be211f2d8c0ef66021
lucastorterotot <lucas.torterotot@gmail.com>
2020-09-08 10:41:47 +0200

    Add Heppy variable names translator

--- python/DL_for_HTT/common/NN_settings.py
+++ python/DL_for_HTT/common/NN_settings.py
@@ -50,3 +50,24 @@ inputs = [
     "mTtt_reco",
     "mTtot_reco",
     ]
+
+inputs_from_Heppy = {
+    "tau1_pt_reco" : "l1_pt",
+    "tau1_eta_reco" : "l1_eta",
+    "tau1_phi_reco" : "l1_phi",
+    "tau2_pt_reco" : "l2_pt",
+    "tau2_eta_reco" : "l2_eta",
+    "tau2_phi_reco" : "l2_phi",
+    "jet1_pt_reco" : "j1_pt",
+    "jet1_eta_reco" : "j1_eta",
+    "jet1_phi_reco" : "j1_phi",
+    "jet2_pt_reco" : "j2_pt",
+    "jet2_eta_reco" : "j2_eta",
+    "jet2_phi_reco" : "j2_phi",
+    "MET_pt_reco" : "met",
+    "MET_phi_reco" : "metphi",
+    "mT1_reco" : "l1_mt",
+    "mT2_reco" : "l2_mt",
+    "mTtt_reco" : "mt_tt",
+    "mTtot_reco" : "mt_tot",
+}
