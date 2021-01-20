a4140fcffc92cbd07eeaeb7d3c84b1d8fe101ac1
Lucas Torterotot <lucas.torterotot@gmail.com>
2021-01-20 08:11:37 +0100

    Improve plots

--- python/DL_for_HTT/post_training/macros.py
+++ python/DL_for_HTT/post_training/macros.py
@@ -124,6 +124,109 @@ def gen_vs_reco(df, channel, model_name, min_mass, max_mass, prefix = '', **kwar
         fig.tight_layout()
         fig.savefig("gen_vs_reco-{}{}.png".format(prefix,var))
 
+def model_response_tau_filtered(df, channel, model_name, min_mass, max_mass, prefix = '', **kwargs):
+    channel = "tt"
+
+    dR_max_gen = 0.1
+    gen_type = "tau"
+    
+    dR_min_jet = 0.5
+    dR_max_jet = 0.1
+
+    pT_max_jet_frac = 2./3
+
+    for filter in ['gen', "r_gen", 'jet', 'r_jet']:
+        df1 = filter_channel(df, channel)
+
+        if filter == 'gen':
+            df1 = df1.loc[df1[gen_type+"1_phi_gen"] != -10 ]
+            df1 = df1.loc[df1[gen_type+"2_phi_gen"] != -10 ]
+            Delta_eta_1 = abs(df1[gen_type+"1_eta_gen"]-df1["tau1_eta_reco"])
+            Delta_eta_2 = abs(df1[gen_type+"2_eta_gen"]-df1["tau2_eta_reco"])
+            Delta_phi_1 = abs(df1[gen_type+"1_phi_gen"]-df1["tau1_phi_reco"])
+            Delta_phi_2 = abs(df1[gen_type+"2_phi_gen"]-df1["tau2_phi_reco"])
+            
+            Delta_phi_1[Delta_phi_1 > np.pi] -= 2*np.pi
+            Delta_phi_2[Delta_phi_2 > np.pi] -= 2*np.pi
+            
+            df1 = df1.loc[(Delta_eta_1**2+Delta_phi_1**2) < dR_max_gen**2]
+            df1 = df1.loc[(Delta_eta_2**2+Delta_phi_2**2) < dR_max_gen**2]
+
+        if filter == 'r_gen':
+            df1 = df1.loc[df1[gen_type+"1_phi_gen"] != -10 ]
+            df1 = df1.loc[df1[gen_type+"2_phi_gen"] != -10 ]
+            Delta_eta_1 = abs(df1[gen_type+"1_eta_gen"]-df1["tau1_eta_reco"])
+            Delta_eta_2 = abs(df1[gen_type+"2_eta_gen"]-df1["tau2_eta_reco"])
+            Delta_phi_1 = abs(df1[gen_type+"1_phi_gen"]-df1["tau1_phi_reco"])
+            Delta_phi_2 = abs(df1[gen_type+"2_phi_gen"]-df1["tau2_phi_reco"])
+            
+            Delta_phi_1[Delta_phi_1 > np.pi] -= 2*np.pi
+            Delta_phi_2[Delta_phi_2 > np.pi] -= 2*np.pi
+            
+            df1 = df1.loc[(Delta_eta_1**2+Delta_phi_1**2) > dR_max_gen**2]
+            df1 = df1.loc[(Delta_eta_2**2+Delta_phi_2**2) > dR_max_gen**2]
+
+        if filter in ["jet", "r_jet"]:
+            Delta_eta_11 = abs(df1["jet1_eta_reco"]-df1["tau1_eta_reco"])
+            Delta_eta_12 = abs(df1["jet1_eta_reco"]-df1["tau2_eta_reco"])
+            Delta_eta_21 = abs(df1["jet2_eta_reco"]-df1["tau1_eta_reco"])
+            Delta_eta_22 = abs(df1["jet2_eta_reco"]-df1["tau2_eta_reco"])
+            Delta_phi_11 = abs(df1["jet1_phi_reco"]-df1["tau1_phi_reco"])
+            Delta_phi_12 = abs(df1["jet1_phi_reco"]-df1["tau2_phi_reco"])
+            Delta_phi_21 = abs(df1["jet2_phi_reco"]-df1["tau1_phi_reco"])
+            Delta_phi_22 = abs(df1["jet2_phi_reco"]-df1["tau2_phi_reco"])
+            
+            Delta_phi_11[Delta_phi_11 > np.pi] -= 2*np.pi
+            Delta_phi_21[Delta_phi_21 > np.pi] -= 2*np.pi
+            Delta_phi_12[Delta_phi_12 > np.pi] -= 2*np.pi
+            Delta_phi_22[Delta_phi_22 > np.pi] -= 2*np.pi
+
+            if filter == "jet":
+                df1 = df1.loc[(Delta_eta_11**2+Delta_phi_11**2) > dR_min_jet**2]
+                df1 = df1.loc[(Delta_eta_12**2+Delta_phi_12**2) > dR_min_jet**2]
+                df1 = df1.loc[(Delta_eta_21**2+Delta_phi_21**2) > dR_min_jet**2]
+                df1 = df1.loc[(Delta_eta_22**2+Delta_phi_22**2) > dR_min_jet**2]
+
+            if filter == "r_jet":
+                df1 = df1.loc[((Delta_eta_11**2+Delta_phi_11**2) < dR_max_jet**2) | ((Delta_eta_12**2+Delta_phi_12**2) < dR_max_jet**2)]
+                df1 = df1.loc[((Delta_eta_21**2+Delta_phi_21**2) < dR_max_jet**2) | ((Delta_eta_22**2+Delta_phi_22**2) < dR_max_jet**2)]
+
+                df1 = df1.loc[
+                    (
+                        (
+                            df1["jet1_pt_reco"] > df1["tau1_pt_reco"] * pT_max_jet_frac
+                        ) & (
+                            df1["jet1_pt_reco"] < df1["tau1_pt_reco"] / pT_max_jet_frac
+                        )
+                    ) | (
+                        (
+                            df1["jet1_pt_reco"] > df1["tau2_pt_reco"] * pT_max_jet_frac
+                        ) & (
+                            df1["jet1_pt_reco"] < df1["tau2_pt_reco"] / pT_max_jet_frac
+                        )
+                    )
+                ]
+
+                df1 = df1.loc[
+                    (
+                        (
+                            df1["jet2_pt_reco"] > df1["tau1_pt_reco"] * pT_max_jet_frac
+                        ) & (
+                            df1["jet2_pt_reco"] < df1["tau1_pt_reco"] / pT_max_jet_frac
+                        )
+                    ) | (
+                        (
+                            df1["jet2_pt_reco"] > df1["tau2_pt_reco"] * pT_max_jet_frac
+                        ) & (
+                            df1["jet2_pt_reco"] < df1["tau2_pt_reco"] / pT_max_jet_frac
+                        )
+                    )
+                ]
+                            
+                            
+        
+        model_response(df1, channel, model_name, min_mass, max_mass, prefix = prefix+"FilterBy"+filter)
+
 def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **kwargs):
     df1 = filter_channel(df, channel)
         
@@ -132,6 +235,11 @@ def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **k
     CL68s_model_do = []
     CL95s_model_up = []
     CL95s_model_do = []
+    medians_model_diff = []
+    CL68s_model_diff_up = []
+    CL68s_model_diff_do = []
+    CL95s_model_diff_up = []
+    CL95s_model_diff_do = []
     medians_mTtot = []
     CL68s_mTtot_up = []
     CL68s_mTtot_do = []
@@ -161,26 +269,34 @@ def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **k
         # mTtots = np.r_[df2["mTtot_reco"]]
         mHs = np.r_[df2[target]]
         values_model = predictions/mHs
+        values_model_diff = predictions - mHs
         # values_mTtot = mTtots/mHs
         
         values_model = [v for v in values_model]
+        values_model_diff = [v for v in values_model_diff]
         # values_mTtot = [v for v in values_mTtot]
         values_model.sort()
+        values_model_diff.sort()
         # values_mTtot.sort()
 
         try:
             medians_model.append(values_model[int(len(values_model)/2)])
+            medians_model_diff.append(values_model_diff[int(len(values_model_diff)/2)])
             # medians_mTtot.append(values_mTtot[int(len(values_mTtot)/2)])
         except:
             import pdb; pdb.set_trace()
 
         above_model = [v for v in values_model if v >= medians_model[-1]]
         below_model = [v for v in values_model if v <= medians_model[-1]]
+        above_model_diff = [v for v in values_model_diff if v >= medians_model_diff[-1]]
+        below_model_diff = [v for v in values_model_diff if v <= medians_model_diff[-1]]
         # above_mTtot = [v for v in values_mTtot if v >= medians_mTtot[-1]]
         # below_mTtot = [v for v in values_mTtot if v <= medians_mTtot[-1]]
 
         above_model.sort()
         below_model.sort(reverse = True)
+        above_model_diff.sort()
+        below_model_diff.sort(reverse = True)
         # above_mTtot.sort()
         # below_mTtot.sort(reverse = True)
 
@@ -188,6 +304,10 @@ def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **k
         CL68s_model_do.append(below_model[int(0.68 * len(below_model))])
         CL95s_model_up.append(above_model[int(0.95 * len(above_model))])
         CL95s_model_do.append(below_model[int(0.95 * len(below_model))])
+        CL68s_model_diff_up.append(above_model_diff[int(0.68 * len(above_model_diff))])
+        CL68s_model_diff_do.append(below_model_diff[int(0.68 * len(below_model_diff))])
+        CL95s_model_diff_up.append(above_model_diff[int(0.95 * len(above_model_diff))])
+        CL95s_model_diff_do.append(below_model_diff[int(0.95 * len(below_model_diff))])
         # CL68s_mTtot_up.append(above_mTtot[int(0.68 * len(above_mTtot))])
         # CL68s_mTtot_do.append(below_mTtot[int(0.68 * len(below_mTtot))])
         # CL95s_mTtot_up.append(above_mTtot[int(0.95 * len(above_mTtot))])
@@ -196,7 +316,7 @@ def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **k
     fig, ax = plt.subplots()
     #fig.suptitle(model_name)
     plt.xlabel("Masse générée du Higgs (GeV)")
-    plt.ylabel("Prédicition du modèle / Masse générée du Higgs (1/GeV)")
+    plt.ylabel("Prédiction du modèle / Masse générée du Higgs")
     
     ax.fill_between(
         xpos, CL95s_model_do, CL68s_model_do,
@@ -264,14 +384,52 @@ def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **k
 
     plt.clf()
     fig, ax = plt.subplots()
+    #fig.suptitle(model_name)
+    plt.xlabel("Masse générée du Higgs (GeV)")
+    plt.ylabel("Prédiction du modèle - Masse générée du Higgs (GeV)")
+    
+    ax.fill_between(
+        xpos, CL95s_model_diff_do, CL68s_model_diff_do,
+        color = "yellow", alpha = .5, label = "$\pm2\sigma$",
+    )
+    ax.fill_between(
+        xpos, CL68s_model_diff_up, CL95s_model_diff_up,
+        color = "yellow", alpha = .5,
+    )
+    ax.fill_between(
+        xpos, CL68s_model_diff_do, CL68s_model_diff_up,
+        color = "green", alpha = .5, label = "$\pm1\sigma$",
+    )
+    ax.errorbar(
+        xpos, medians_model_diff, xerr = xerr, #yerr = sigmas,
+        marker='.', markersize=4, linewidth=0, elinewidth=1,
+        fmt=' ', capsize = 3, capthick = 0, color = "black", label = "Médiane",
+    )
+    
+    plt.plot([min_mass, max_mass], [0,0], color='C3')    
+
+    plt.ylim(-500,500)
+    plt.xlim(min_mass, max_mass)
+
+    ax.legend(loc='upper right')
+    
+    fig.tight_layout()
+    fig.savefig("model_response_diff-{}{}.png".format(prefix,model_name))
+
+    plt.xlim(min_mass, 200)
+    plt.xticks(np.arange(min_mass, 201, step=10))
+    fig.savefig("model_response_diff_lowmass-{}{}.png".format(prefix,model_name))
+
+    plt.clf()
+    fig, ax = plt.subplots()
 
     plt.xlabel("Masse générée du Higgs (GeV)")
-    plt.ylabel("Résolution relative normée du modèle")
+    plt.ylabel("Réponse calibrée du modèle")
 
-    CL68s_model_up = [CL68s_model_up[k]/medians_model[k]-1 for k in range(len(medians_model))]
-    CL68s_model_do = [CL68s_model_do[k]/medians_model[k]-1 for k in range(len(medians_model))]
-    CL95s_model_up = [CL95s_model_up[k]/medians_model[k]-1 for k in range(len(medians_model))]
-    CL95s_model_do = [CL95s_model_do[k]/medians_model[k]-1 for k in range(len(medians_model))]
+    CL68s_model_up = [CL68s_model_up[k]/medians_model[k] for k in range(len(medians_model))]
+    CL68s_model_do = [CL68s_model_do[k]/medians_model[k] for k in range(len(medians_model))]
+    CL95s_model_up = [CL95s_model_up[k]/medians_model[k] for k in range(len(medians_model))]
+    CL95s_model_do = [CL95s_model_do[k]/medians_model[k] for k in range(len(medians_model))]
     
     ax.fill_between(
         xpos, CL95s_model_do, CL68s_model_do,
@@ -286,15 +444,15 @@ def model_response(df, channel, model_name, min_mass, max_mass, prefix = '', **k
         color = "green", alpha = .5, label = "$\pm1\sigma$",
     )
 
-    plt.plot([min_mass, max_mass], [0,0], color='C3')    
+    plt.plot([min_mass, max_mass], [1,1], color='C3')    
 
-    plt.ylim(-1,2)
+    plt.ylim(0,3)
     plt.xlim(min_mass, max_mass)
 
     ax.legend(loc='upper right')
     
     fig.tight_layout()
-    fig.savefig("model_resolution-{}{}.png".format(prefix,model_name))
+    fig.savefig("model_response_calibrated-{}{}.png".format(prefix,model_name))
 
     plt.close('all')
 
@@ -530,4 +688,5 @@ available_plots = {
     'variables_distributions': variables_distributions,
     'predictions_distributions' : predictions_distributions,
     'gen_vs_reco' : gen_vs_reco,
+    'model_response_tau_filtered' : model_response_tau_filtered,
 }
